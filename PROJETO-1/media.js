8c94a0b9f436790e45c153d50471b7b6fef22dc2
reisfellipe <reisfellipe11@gmail.com>
2023-06-20 21:56:31 -0300

    projeto-Figma

---
+++ PROJETO-1/media.js
@@ -0,0 +1,5 @@
+const btnMobile = document.querySelector('.btn-mobile')
+btnMobile.addEventListener('click', function(){
+    const menu = document.querySelector('.menu')
+    menu.classList.toggle('active')
+})
